6cbc2cf72ba051ac43876a4c5ce5a0d548425013
nakaji <nakaji126@gmail.com>
2014-09-18 01:23:46 +0900

    intial commit

---
+++ ElectionTool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElectionTool.MvcApplication" Language="C#" %>
---
+++ ElectionTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectionTool.Startup))]
+namespace ElectionTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
