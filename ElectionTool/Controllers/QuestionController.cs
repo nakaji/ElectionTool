8646bd83b2af7365cab732fb37d32734fe1f401d
nakaji <nakaji126@gmail.com>
2014-09-18 01:47:24 +0900

    質問登録用のコントローラとビューを追加

---
+++ ElectionTool/Controllers/QuestionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElectionTool.Models;
+
+namespace ElectionTool.Controllers
+{
+    public class QuestionController : Controller
+    {
+        [HttpPost]
+        public ActionResult Entry(HomeIndexViewModel model)
+        {
+            // todo:Twitterへの認証チェック
+
+            // todo:データベースへの登録
+
+            // todo:Twitterへの投稿
+
+            return View();
+        }
+    }
+}
