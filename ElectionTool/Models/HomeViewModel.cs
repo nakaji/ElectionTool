352ebc9a50508fbee6d33a20cf0762167f150407
nakaji <nakaji126@gmail.com>
2014-09-18 01:29:24 +0900

    トップページの大枠をざっくり作成

---
+++ ElectionTool/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElectionTool.Models
+{
+    public class HomeIndexViewModel
+    {
+        [MaxLength(100)]
+        [Display(Name="質問")]
+        public string Question { get; set; }
+    }
+}
