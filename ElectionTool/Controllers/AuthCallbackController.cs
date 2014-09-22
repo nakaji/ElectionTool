5b646681f7fccb8bb3a2bc9c0308b4dd44db6490
nakaji <nakaji126@gmail.com>
2014-09-23 01:38:57 +0900

    Twitter認証とツイート機能を追加

---
+++ ElectionTool/Controllers/AuthCallbackController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using CoreTweet;
+using Microsoft.Owin.Security.Twitter.Messages;
+
+namespace ElectionTool.Controllers
+{
+    public class AuthCallbackController : Controller
+    {
+        // GET: Auth
+        public ActionResult Twitter(string oauth_token, string oauth_verifier)
+        {
+            var oAuthSession = Session["OAuthSession"] as OAuth.OAuthSession;
+            var token = oAuthSession.GetTokens(oauth_verifier);
+            Session["AccessToken"] = token;
+
+            var question = Session["Question"] as string;
+
+            return Redirect("~/Question/Entry?question=" + question);
+        }
+    }
+}
--- ElectionTool/Controllers/QuestionController.cs
+++ ElectionTool/Controllers/QuestionController.cs
@@ -1,24 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CoreTweet;
 using ElectionTool.Models;
 
 namespace ElectionTool.Controllers
 {
     public class QuestionController : Controller
     {
-        [HttpPost]
-        public ActionResult Entry(HomeIndexViewModel model)
+        public ActionResult Entry(string question)
         {
+            var token = Session["AccessToken"] as Tokens;
+            if (token != null)
+            {
+                // todo:Twitterへの投稿
+                token.Statuses.Update(status => question);
+                // todo:データベースへの登録
+
+
+                return View();
+            }
+
             // todo:Twitterへの認証チェック
+            var consumerKey = ConfigurationManager.AppSettings["TwitterApiKey"];
+            var consumerSecret = ConfigurationManager.AppSettings["TwitterApiKeySecret"];
 
-            // todo:データベースへの登録
+            var oAuthSession = OAuth.Authorize(consumerKey, consumerSecret, "http://mydomain.com:63543/AuthCallback/Twitter");
 
-            // todo:Twitterへの投稿
-
-            return View();
+            Session["OAuthSession"] = oAuthSession;
+            Session["Question"] = question;
+            
+            return Redirect(oAuthSession.AuthorizeUri.OriginalString);
         }
     }
 }
