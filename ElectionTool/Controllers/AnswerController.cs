0fec7c45bfe413dd1ea51b693b2bba50162eb953
nakaji <nakaji126@gmail.com>
2014-09-25 23:48:28 +0900

    回答用の質問一覧ページを追加

---
+++ ElectionTool/Controllers/AnswerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElectionTool.Models;
+
+namespace ElectionTool.Controllers
+{
+    public class AnswerController : Controller
+    {
+        private AppDbContext _db = new AppDbContext();
+
+        // GET: Answer
+        public ActionResult Index()
+        {
+            var model = new AnswerIndexViewModel();
+            model.Questions = _db.Questions;
+
+            return View(model);
+        }
+    }
+}
---
+++ ElectionTool/Models/AnswerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectionTool.Models
+{
+    public class AnswerIndexViewModel
+    {
+        public IEnumerable<Question> Questions { get; set; }
+    }
+}
