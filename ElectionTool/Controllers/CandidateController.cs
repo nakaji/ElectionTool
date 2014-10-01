fdd7cb041623a4652de06fc6a93b32d554a49e85
nakaji <nakaji126@gmail.com>
2014-10-02 00:45:33 +0900

    タイムライン表示（仮実装）

---
+++ ElectionTool/Controllers/CandidateController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ElectionTool.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ElectionTool.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private AppDbContext _db = new AppDbContext();
+
+        // GET: Timeline
+        public ActionResult Timeline(string userId)
+        {
+            if (userId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var questions = _db.Questions.Include("Answers").Where(x => x.Answers.Any(a => a.UserId == userId));
+            return PartialView(questions);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
