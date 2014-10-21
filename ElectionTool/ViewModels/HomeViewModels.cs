66f310c9cbec6020c881818ef603ea78081b585f
nakaji <nakaji126@gmail.com>
2014-10-22 05:04:47 +0900

    回答数を表示 fix #18

--- ElectionTool/ViewModels/HomeViewModels.cs
+++ ElectionTool/ViewModels/HomeViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Configuration;
+using System.Linq;
 using ElectionTool.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -9,10 +10,10 @@ namespace ElectionTool.ViewModels
     public class HomeIndexViewModel
     {
         [MaxLength(100)]
-        [Display(Name="質問")]
+        [Display(Name = "質問")]
         public string Question { get; set; }
 
-        public IEnumerable<ApplicationUser> Users { get; private set; }
+        public List<CandidateInfo> CandidateInfos { get; private set; }
 
         public string TwitterWidgetId { get; private set; }
 
@@ -20,9 +21,28 @@ namespace ElectionTool.ViewModels
 
         public HomeIndexViewModel()
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
-            Users = manager.Users;
+            CandidateInfos = new List<CandidateInfo>();
+            using (var db = new ApplicationDbContext())
+            {
+                var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(db));
+                var answers = db.Answers.GroupBy(x => x.UserId).ToList();
+                foreach (var user in manager.Users)
+                {
+                    var replies = answers.FirstOrDefault(x => x.Key == user.Id);
+                    CandidateInfos.Add(new CandidateInfo()
+                    {
+                        User = user,
+                        ReplyCount = replies == null ? 0 : replies.Count()
+                    });
+                }
+            }
             TwitterWidgetId = ConfigurationManager.AppSettings["TwitterWidgetId"];
+        }
+
+        public class CandidateInfo
+        {
+            public ApplicationUser User { get; set; }
+            public int ReplyCount { get; set; }
         }
     }
 }
